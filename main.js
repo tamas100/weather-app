0e87104dee3cca5882d9b6927b6d48eadcac0b2c
Tamás Kis <t.kis502@gmail.com>
2023-10-31 18:05:39 +0100

    write location detection and render function, delete unnecessary comments

--- main.js
+++ main.js
@@ -2,24 +2,11 @@ const $form = document.querySelector(".js-city-form");
 const $container = document.querySelector(".js-city-weather");
 const $searchInput = document.querySelector("[name=city]");
 const $errorSection = document.querySelector(".error");
-// const apiUrl = `https://api.weatherapi.com/v1/current.json?key=14be385073aa4d85a9773012232610&q=${city}&aqi=no`;
 
 function getLocation(location) {
     return `https://api.weatherapi.com/v1/forecast.json?key=14be385073aa4d85a9773012232610&q=${location}&aqi=no&lang=hu&days=5`;
-
 }
 
-// function showPosition(position) {
-//     let latitude = position.coords.latitude;
-//     let longitude = position.coords.longitude;
-//     console.log(`
-//     Latitude: ${latitude}
-//     Longitude: ${longitude}
-//     `);
-//     let location = `${latitude},${longitude}`
-//     getLocation(location);
-//     return location;
-// }
 
 function fetchPosition(position) {
     let latitude = position.coords.latitude;
@@ -27,23 +14,32 @@ function fetchPosition(position) {
     let location = `${latitude},${longitude}`;
     fetch(getLocation(location))
         .then(response => response.json())
-        .then(response => console.log(response))
-        .catch(error => console.log(error.message));
+        .then(renderResponse);
 }
 
 const position = navigator.geolocation.getCurrentPosition(fetchPosition);
 
-function renderResponse(weather) {
-    console.log(weather);
-    let html = ``;
-    // validálás, sikertelen keresésnél error az objektum egyetlen kulcsa
-    if (!weather.error) {
-        // html += `<div class="weather-div">${renderWeather(weather)}</div>`;
-        html += `<div class="weather-div">Sikerült!</div>`;
-    } else {
-        $errorSection.innerHTML = `<p>Sorry, we couldn't find the city!</p>`;
-    }
+function renderWeather(weather) {
+    let html = `
+        <h2>${weather.location.name}</h2>
+        <p>${weather.current.temp_c}°C</p>
+        <p>${weather.forecast.forecastday[0].day.mintemp_c}°C - ${weather.forecast.forecastday[0].day.maxtemp_c}°C</p>
+        <p>Hőérzet: ${weather.current.feelslike_c}°C</p>
+        <p>Szélsebesség: ${weather.current.wind_kph}km/h</p>
+        <p>${weather.current.condition.text}</p>
+        <img src="https:${weather.current.condition.icon}" alt="${weather.current.condition.text}"/>
+    `;
+    return html;
+}
 
+function renderResponse(weather) {
+    let html = ``;
+    // validálás, sikertelen keresésnél "error" az objektum egyetlen kulcsa
+    if (!weather.error) {
+        html += `<div class="weather-div">${renderWeather(weather)}</div>`;
+    } else {
+        $errorSection.innerHTML = `<p>Sajnálom, nem találok ilyen nevű települést!</p>`;
+    }
     $container.innerHTML = html;
 }
 
@@ -55,11 +51,7 @@ function fetchCity(city) {
     fetch(getApiUrl(city))
         .then(response => response.json())
         .then(renderResponse);
-    // .then(response => console.log(response))
-    // .catch(error => console.log(error.message));
 }
-
-// fetchCity("Sopnort");
 
 function formSubmitted(event) {
     // Megakadályozzuk az oldal újratöltődését
@@ -77,10 +69,7 @@ function formSubmitted(event) {
     } else {
         $errorSection.innerHTML = 'Sikertelen keresés!'
     }
-    console.log(city)
 }
-
-
 
 $form.addEventListener("submit", formSubmitted);
 
